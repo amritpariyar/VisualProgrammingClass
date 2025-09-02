6ac2c2d8465be68e165d726238331b342433a010
Amrit Pariyar <amrit@norbrik.com>
2025-09-02 07:14:10 +0545

    Add ConsoleApp2 project with core functionality
    - Introduced a new project "ConsoleApp2" in the solution.
    - Created `App.config` for configuration settings.
    - Added `ConsoleApp2.csproj` with project properties and references.
    - Implemented `Program.cs` to demonstrate `StudentInfo` usage.
    - Created `AssemblyInfo.cs` for assembly metadata.
    - Defined `StudentInfo.cs` class with properties and methods.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //class varialname = new class();
+      //studentInfo is class
+      //student objet
+      StudentInfo student = new StudentInfo(); // create an instance of StudentInfo class
+      student.Name = "John"; // set Name property
+      student.Age = 20; // set Age property
+
+      string info = student.GetInfo(); // call GetInfo method
+      Console.WriteLine("Hello " + info); // output to console
+
+
+      StudentInfo student2 = new StudentInfo(); // create an instance of StudentInfo class
+      student2.Name = "Ramesh"; // set Name property
+      student2.Age = 30; // set Age property
+
+      info = student2.GetInfo(); // call GetInfo method
+
+
+      Console.WriteLine("Hello " + info); // output to console
+    }
+  }
+}
---
+++ ConsoleApp2/StudentInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+  //private, protected, internal, public, sealed, abstract
+  internal class StudentInfo
+  {
+    //fields
+    private string name;
+    private int age;
+
+    //properties
+    public string Name
+    {
+      get
+      {
+        return "Mister "+ name;
+      }
+      set
+      {
+        name = value;
+      }
+    }
+    //public string Name { get; set; }
+    public int Age
+    {
+      get
+      {
+        return age;
+      }
+      set
+      {
+        age = value;
+      }
+    }
+
+    public string GetInfo()
+    {
+      return $"Name: {Name}, Age: {Age}";
+    }
+
+    //constructor
+    //public StudentInfo(string name, int age)
+    //{
+    //  Name = name;
+    //  Age = age;
+    //}
+
+    //methods
+    //destructor
+  }
+}
